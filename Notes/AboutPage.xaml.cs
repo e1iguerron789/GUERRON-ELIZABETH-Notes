95bae86b94b145ed6b6df03cbe3f94402abc93b7
e1iguerron789 <130955581+e1iguerron789@users.noreply.github.com>
2024-12-01 22:55:35 -0500

    mas cambios

--- Notes/AboutPage.xaml.cs
+++ Notes/AboutPage.xaml.cs
@@ -4,6 +4,12 @@ public partial class AboutPage : ContentPage
 {
 	public AboutPage()
 	{
-		InitializeComponent();
+        InitializeComponent();
 	}
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+
 }
