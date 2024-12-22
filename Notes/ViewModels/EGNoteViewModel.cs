91a1a9c323e5459d8e56e40332276d0a276ff773
e1iguerron789 <130955581+e1iguerron789@users.noreply.github.com>
2024-12-22 15:20:07 -0500

    Cambios en  AboutPage.xaml

--- Notes/ViewModels/EGNoteViewModel.cs
+++ Notes/ViewModels/EGNoteViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Notes.ViewModels
 {
-    internal class NotesViewModel
+    internal class EGNoteViewModel
     {
     }
 }
--- Notes/ViewModels/EGNotesViewModel.cs
+++ Notes/ViewModels/EGNotesViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Notes.ViewModels
 {
-    internal class AboutViewModel
+    internal class EGNotesViewModel
     {
     }
 }
